443d797327ac7170029203fd18af7296d2cfad5d
wahyuvlntn <wahyuvalentino@student.ub.ac.id>
2023-09-22 16:33:46 +0700

    wahyu push lagi

--- app/src/main/java/com/example/pomodoroproject/MainActivity.kt
+++ app/src/main/java/com/example/pomodoroproject/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android User 2")
+                    Greeting("Android User 2 lagi")
                 }
             }
         }
